aea4892c612d77ba497ac7cdb42f8059fb2c79a6
Nitu9355 <sn169779@gmail.com>
2025-08-27 02:35:33 +0600

    php

---
+++ php/area.php
@@ -0,0 +1,9 @@
+<?php
+$legth = 10;
+$width = 5;
+$area = $legth * $width;
+$perimeter = 2* ($legth +$width);
+echo "Area of Rectangle = $area <br>";
+echo "Perimeter of Rectangle = $perimeter";
+?>
+ 
